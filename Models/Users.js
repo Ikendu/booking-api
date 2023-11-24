cbe1bc207fca2b10b7eb7c3a70e782a169d461ee
Chibunud <ndubest56@gmail.com>
2023-11-24 05:42:54 +0100

    database models

---
+++ Models/Users.js
@@ -0,0 +1,12 @@
+const mongoose = require(`mongoose`)
+const { model, Schema } = mongoose
+
+const UserSchema = new Schema({
+  name: String,
+  email: { type: String, require: true, unique: true },
+  password: { type: String, required: true },
+})
+
+const User = model(`User`, UserSchema)
+
+module.exports = User
